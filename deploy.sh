c83d106603e662b90c703a476c49b62476a33c44
Eetay Natan <eetay2@gmail.com>
2019-01-05 21:43:39 -0800

    cloudformation stack and deploy scipt

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+#if [ -z "$AWS_EXEC" ]; then source ./aws-exec.sh ""; fi
+ACTION=$1
+MYDIR=`dirname $0`
+STACK=FinancialAnalyzerStack
+
+case $ACTION in
+  update|create)
+    aws cloudformation ${ACTION}-stack --stack-name $STACK \
+      --template-body file://./cloudformation.yaml \
+      --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM 
+    ./cloudformation-tail.sh $STACK $AWS_REGION $AWS_PROFILE
+    ;;
+  deploy)
+    ZIP=taseGetInfoLambda.zip
+    pushd taseGetInfoLambda > /dev/null && zip ../dist/$ZIP -r . && popd > /dev/null
+    aws lambda update-function-code --function-name TaseGetInfoLambda --zip-file fileb://./dist/$ZIP
+    ;;
+  delete)
+    aws cloudformation delete-stack --stack-name $STACK
+    ./cloudformation-tail.sh $STACK $AWS_REGION $AWS_PROFILE
+    ;;
+  *)
+    echo "Usage: $0 [create|delete|update]"
+    ;;
+esac
