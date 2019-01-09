fce709dc33d04f640cf65a73ce0cfdced01fa5c3
Eetay Natan <eetay2@gmail.com>
2019-01-08 18:38:25 -0800

    support multiple stage environments deployment

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,22 @@
 #if [ -z "$AWS_EXEC" ]; then source ./aws-exec.sh ""; fi
 ACTION=$1
 MYDIR=`dirname $0`
-STACK=FinancialAnalyzerStack
+STAGE=Dev
+STACK=FinancialAnalyzerStack$STAGE
 
 case $ACTION in
   update|create)
     aws cloudformation ${ACTION}-stack --stack-name $STACK \
       --template-body file://./cloudformation.yaml \
-      --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM 
+      --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+      --parameters \
+        ParameterKey=Stage,ParameterValue=$STAGE
     ./cloudformation-tail.sh $STACK $AWS_REGION $AWS_PROFILE
     ;;
   deploy)
     ZIP=taseGetInfoLambda.zip
     pushd taseGetInfoLambda > /dev/null && zip ../dist/$ZIP -r . && popd > /dev/null
-    aws lambda update-function-code --function-name TaseGetInfoLambda --zip-file fileb://./dist/$ZIP
+    aws lambda update-function-code --function-name $STACK-TaseGetInfoLambda --zip-file fileb://./dist/$ZIP
     ;;
   delete)
     aws cloudformation delete-stack --stack-name $STACK
